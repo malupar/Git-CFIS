36c4dbf4cde37622b860a259012cb7688bb6102c
malupar <apariciogarciamarialucia@gmail.com>
2026-04-19 22:37:11 +0200

    Multiplicacion y division sin implementar

--- src/app.py
+++ src/app.py
@@ -3,3 +3,9 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+
+def multiply(a, b):
+    raise NotImplementedError("Queda por implementar esta funcion")
+
+def divide(a, b):
+    raise NotImplementedError("Queda por implementar esta funcion")
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
-from src.app import add, subtract
+from src.app import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
@@ -7,3 +7,15 @@ def test_add():
 def test_subtract():
     assert subtract(10, 5) == 5
     assert subtract(0, 5) == -5
+
+def test_multiply():
+    assert multiply(10, 5) == 50
+    assert multiply(0, 5) == 0
+    assert multiply(-1, -1) == 1
+    assert multiply(-1, 5) == -5
+
+def test_divide():
+    assert divide(10, 5) == 2
+    assert divide(20, 3) == 6
+    assert divide(-1, -1) == 1
+    assert divide(-1, 5) == 0
