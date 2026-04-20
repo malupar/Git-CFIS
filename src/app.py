48749d416cdb5aa634863c60fe4973dabcbe8526
daviddenamiel <david.denamiel@estudiantat.upc.edu>
2026-04-20 16:22:44 +0200

    Arreglo test

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ def subtract(a, b):
     return a - b
 
 def multiply(a, b):
-    raise NotImplementedError("Queda por implementar esta funcion")
+    return a * b
 
 def divide(a, b):
-    raise NotImplementedError("Queda por implementar esta funcion")
+    return a // b
