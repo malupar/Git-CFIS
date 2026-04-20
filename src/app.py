364e065adef1e034b06437d1f4b22ccc5c37eb84
dmdeux <dmdeux@gmail.com>
2026-04-20 16:21:07 +0200

    arreglao

--- src/app.py
+++ src/app.py
@@ -5,7 +5,9 @@ def subtract(a, b):
     return a - b
 
 def multiply(a, b):
-    raise NotImplementedError("Queda por implementar esta funcion")
+    return a*b
+    #raise NotImplementedError("Queda por implementar esta funcion")
 
 def divide(a, b):
-    raise NotImplementedError("Queda por implementar esta funcion")
+    return a//b
+    #raise NotImplementedError("Queda por implementar esta funcion")
